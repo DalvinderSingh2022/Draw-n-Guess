1d63666e837d54647ddcfd6201df467bca73892c
DalvinderSingh2022 <dalvinderSingh201999@gmail.com>
2024-06-27 00:22:36 +0530

    Modal Styling changes

--- client/src/Modal.js
+++ client/src/Modal.js
@@ -16,9 +16,12 @@ const Modal = () => {
     return (
         alert ?
             <div className='fixed_container justify-center bg-blue-200/50 items-center'>
-                <div className='mx-4 bg-white min-w-96 border-b-4 border-yellow-400 rounded-xl px-12 py-6 flex items-center flex-col gap-6 animate-popin'>
-                    <div className='text-2xl capitalize font-semibold text-yellow-500 text-center'>{alert}</div>
-                    <button onClick={() => setAlert(false)} className='button primary rounded-2xl px-6 py-2'>Okay</button>
+                <div className='mx-4 bg-white min-w-96 border-b-4 border-orange-400 rounded-2xl flex items-center flex-col animate-popin'>
+                    <div className="heading secondary">Alert</div>
+                    <div className="flex flex-col items-center px-12 gap-6 py-4">
+                        <div className='text-2xl capitalize font-semibold text-orange-500 text-center'>{alert}</div>
+                        <button onClick={() => setAlert(false)} className='button secondary rounded-xl px-3 py-1.5'>Okay</button>
+                    </div>
                 </div>
             </div>
             : null
